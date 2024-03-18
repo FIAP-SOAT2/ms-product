2077572e7f962e560f71d341fdee5f389c254bdc
jlsjeffers <jlsjefferson@gmail.com>
2024-03-18 16:10:20 -0300

    feat/AddReserveStockFlow

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import dotenv from 'dotenv';
 import setupApp from './main/config/app';
+import AwsSQS from './infra/aws/sqs.consumer';
 
 dotenv.config();
 
 const PORT = parseInt(`${process.env.PORT || 3000}`);
+
+
+async function startSQSConsumer() {
+  const sqsConsumer = new AwsSQS();
+
+  try {
+    await sqsConsumer.consumeFromQueue();
+  } catch (error) {
+    console.error('Failed to consume from SQS:', error);
+  }
+}
+setInterval(startSQSConsumer, 1000);
 
 const app = setupApp();
 app.listen(PORT, () => {
