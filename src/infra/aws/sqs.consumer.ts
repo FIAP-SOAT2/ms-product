b88da2363276c64c1c78bc35ce3d545af6e0f021
Geowana Siqueira <geowana@motorola.com>
2024-03-18 17:28:07 -0300

    Running in aws

--- src/infra/aws/sqs.consumer.ts
+++ src/infra/aws/sqs.consumer.ts
@@ -1,30 +1,24 @@
-
 import { ProductRepository } from '../database/repositories/ProductRepository';
 import { UpdateProductInterface } from '../../application/interfaces/use-cases/product/UpdateProductInterface';
 import { UpdateProduct } from '../../application/use-cases/product/UpdateProduct';
 import AWS from 'aws-sdk';
 
-const defaultRegion = process.env.AWS_REGION || 'us-central-1';
+const defaultRegion = process.env.AWS_REGION || 'us-east-1';
 const queueUrl = process.env.ORDER_STOCK_QUEUE;
 
 export default class AwsSQS {
   private sqs: AWS.SQS;
 
   constructor(
-   private readonly updateOrder?: UpdateProduct,
+    private readonly updateOrder?: UpdateProduct,
     region?: string,
   ) {
     const options: AWS.SQS.ClientConfiguration = {
       region: region || defaultRegion,
-       endpoint: process.env.LOCALSTACK_URL || 'http://localhost:4566',
-       credentials: {
-       accessKeyId: 'dummy',
-       secretAccessKey: 'dummy',
-       }
     };
-    const productRepostory = new ProductRepository()
+    const productRepostory = new ProductRepository();
     this.sqs = new AWS.SQS(options);
-    this.updateOrder = new UpdateProduct(productRepostory,productRepostory);
+    this.updateOrder = new UpdateProduct(productRepostory, productRepostory);
   }
 
   async deleteMessage(receiptHandle) {
@@ -36,26 +30,33 @@ export default class AwsSQS {
   }
 
   public async consumeFromQueue(): Promise<any> {
-    const params = {
-      QueueUrl: queueUrl,
-      MaxNumberOfMessages: 10,
-      WaitTimeSeconds: 5,
-    };
-    const data = await this.sqs.receiveMessage(params).promise();
-    if (data.Messages.length === 0) {
-      return;
-    }
-    const consumeData = JSON.parse(JSON.parse(data.Messages[0].Body).Message);
-    await consumeData.map(async (order) => {
-      const updateObject = {
-        productId: order.productId,
-        productData: {
-          stock: order.quantity,
-        },
+    try {
+      const params = {
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: 10,
+        WaitTimeSeconds: 5,
       };
-      await this.updateOrder.execute(updateObject,'stock_reserved');
-    });
-    await this.deleteMessage(data.Messages[0].ReceiptHandle);
-    return data;
+      const data = await this.sqs.receiveMessage(params).promise();
+      if (data.Messages.length === 0) {
+        console.log('No message');
+        return;
+      }
+      const consumeData = JSON.parse(JSON.parse(data.Messages[0].Body).Message);
+      console.log('consumeData', consumeData);
+
+      await consumeData.map(async (order) => {
+        const updateObject = {
+          productId: order.productId,
+          productData: {
+            stock: order.quantity,
+          },
+        };
+        await this.updateOrder.execute(updateObject, 'stock_reserved');
+      });
+      await this.deleteMessage(data.Messages[0].ReceiptHandle);
+      return data;
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 }
